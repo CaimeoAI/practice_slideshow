30551d7ff6a5c40237ecfcff5f1e468823b7b369
CaimeoAI <judiellezinke@yahoo.de>
2025-01-30 15:55:12 +0100

    added returns of divs when loading is in progress as well as when an error has occured. Also added several comments for better clarification

--- src/components/slideshow.jsx
+++ src/components/slideshow.jsx
@@ -1,18 +1,25 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 
 export default function Slideshow() {
 
+    //? Stores the fetched images
     const [images, setImages] = useState([])
+    //? Tacks which slide is currently display
     const [currentSlide, setCurrentSlide] = useState(0)
+    //? Stores error messages if the fetch request fails
     const [errorMsg, setErrorMsg] = useState(null)
+    //? Tracks whether the fetch request is in progress
     const [loading, setLoading] = useState(false)
 
+    //? This function is asynchronous, meaning it performs network requests without blocking execution
     async function fetchImages(getUrl) {
         try{
 
+            //? Fetching data from the given URL
             const response = await fetch(getUrl)
+            //? Converts the reponse from the API into json format so I can use it properly
             const data = await response.json()
 
             if(data) {
@@ -29,6 +36,14 @@ export default function Slideshow() {
     useEffect(() => {
         if(url !== '') fetchImages()
     },[url])
+
+    if(loading) {
+        return <div>Loading data in progress...</div>
+    }
+
+    if(errorMsg !== null) {
+        return <div>Error occured: {errorMsg}</div>
+    }
 
     return (
         <div className="slideshow-wrapper">
